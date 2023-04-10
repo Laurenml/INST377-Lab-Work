ee3f73f20c6dd7aec6e71ddf3e2c904faa65132a
Laurenml <lliberat@terpmail.umd.edu>
2023-04-10 13:41:26 -0400

    lab 8 changes

--- client/lab_8/hints.js
+++ client/lab_8/hints.js
@@ -24,15 +24,6 @@ function injectHTML(list) {
 
 }
 
-/*function processRestaurants(list) {
-  console.log('fired restaurants list');
-  const range = [...Array(15).keys()];
-  const newArray = range.map((item) => {
-    const index = getRandomIntInclusive(0, list.length);
-    return list[index]
-  })
-return newArray;*/
-
 function filterList(array, filterInputValue){
   const newArray = array.filter((item) => {
       const lowerCaseName = item.name.toLowerCase();
@@ -53,20 +44,51 @@ function cutRestaurantList(list) {
 
 }
 
+function initMap() {
+  const carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(carto);
+return carto;
 
+}
+
+function markerPlace(array, map){
+  console.log('array for markers', array);
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+
+  array.forEach((item) => {
+    console.log('markerPlace', item);
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+
+  })
+    
+
+}
 
 async function mainEvent() {
   const mainForm = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const loadDataButton = document.querySelector('#data_load');
+  const clearDataButton = document.querySelector('#data_clear');
   const generateListButton = document.querySelector('#generate')
   const textField = document.querySelector('#resto');
   const loadAnimation = document.querySelector('#data_load_animation') //.lds-ellipsis
   loadAnimation.style.display = 'none'; // let your submit button disappear
   generateListButton.classList.add('hidden');
 
+  const carto = initMap();
+
   const storedData = localStorage.getItem('storedData');
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0){
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0){
     generateListButton.classList.remove('hidden')
   }
 
@@ -74,17 +96,6 @@ async function mainEvent() {
   let currentList = [];
   
 
-  console.log(`replace me with the first entry`);
-
- 
-  console.log(`replace me with the name and category of the first entry`);
-
-  // This IF statement ensures we can't do anything if we don't have information yet
-//  if (arrayFromJson?.length > 0) { // the question mark in this means "if this is set at all"
-//    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
-//}
-// loadAnimation.classList.remove('lds-ellipsis');
-// loadAnimation.classList.add('lds-ellipsis_hidden');
 
    
   loadDataButton.addEventListener('click', async (submitEvent) => {
@@ -94,8 +105,11 @@ async function mainEvent() {
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
       const storedList = await results.json();
       localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList;
       
-            
+      if (parsedData?.length > 0) {
+        generateListButton.classList.remove("hidden")
+      }      
   
 
       loadAnimation.style.display = 'none';
@@ -104,28 +118,31 @@ async function mainEvent() {
     
 
 
- 
-
   generateListButton.addEventListener('click', (event) => {
       console.log('generate new list');
-      currentList = cutRestaurantList(storedList);
+      currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto);
 })
 
   textField.addEventListener('input', (event)=> {
       console.log('input', event.target.value);
       const newList = filterList(currentList, event.target.value);
       console.log(newList);
-      injectHTML(newList);        
+      injectHTML(newList);  
+      markerPlace(newList, carto);      
+  });
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('localStorage Check', localStorage.getItem("storedData"));
   })
 })
 
-/*
-  This last line actually runs first!
-  It's calling the 'mainEvent' function at line 57
-  It runs first because the listener is set to when your HTML content has loaded
-*/
+}
+
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
 
 // Leaflet can be a bit old-fashioned.
@@ -134,4 +151,4 @@ document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the a
 /* map.eachLayer((layer) => {
  // if (layer instanceof L.Marker) {
    // layer.remove();
-  } */}
+  } */
